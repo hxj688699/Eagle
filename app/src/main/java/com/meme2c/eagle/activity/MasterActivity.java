e791369499b2d05664da23635729d6f301f264f5
HXJ <hxj688699@gmail.com>
2016-09-17 22:27:56 +0800

    init meme2c

---
+++ app/src/main/java/com/meme2c/eagle/activity/MasterActivity.java
@@ -0,0 +1,19 @@
+package com.meme2c.eagle.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.meme2c.eagle.R;
+
+public class MasterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master);
+        Butter
+
+    }
+
+
+}
